7accbcd9c9e6131d47e08fe15101674a89dda452
MateusVasc <109766889+MateusVasc@users.noreply.github.com>
2024-07-29 20:22:59 -0300

    Renaming python method

--- Arrays/Easy/2798 Number of Employees Who Met the Target/Solution.py
+++ Arrays/Easy/2798 Number of Employees Who Met the Target/Solution.py
@@ -1,6 +1,6 @@
 class Solution(object):
-    def numberOfEmployeesWhoMetTarget(self, hours, target):
+    def number_of_employees_who_met_target(self, hours, target):
         if len(hours) == 1:
             return 1 if hours[0] >= target else 0
         
-        return self.numberOfEmployeesWhoMetTarget(hours[:len(hours)//2], target) + self.numberOfEmployeesWhoMetTarget(hours[len(hours)//2:], target)
+        return self.number_of_employees_who_met_target(hours[:len(hours)//2], target) + self.number_of_employees_who_met_target(hours[len(hours)//2:], target)
