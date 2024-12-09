977f9c7e43a771bca2b149f69eac62b5e2e43573
MateusVasc <mvaraujo2004@gmail.com>
2024-12-09 13:39:20 -0300

    Adds better cpp solution for problem 1

--- Algorithms/Easy/1 Two Sum/Solution.cpp
+++ Algorithms/Easy/1 Two Sum/Solution.cpp
@@ -1,4 +1,5 @@
 #include <vector>
+#include <unordered_map>
 using namespace std;
 
 class Solution {
@@ -21,5 +22,23 @@ public:
         }
 
         return out;
+    }
+
+    vector<int> twoSumBetter(vector<int>& nums, int target) {
+        unordered_map<int, int> map;
+
+        for (int i = 0; i < nums.size(); i++)
+        {
+            int complement = target - nums[i];
+
+            if (map.find(complement) != map.end())
+            {
+                return {map[complement], i};
+            }
+            
+            map[nums[i]] = i;
+        }
+        
+        return {};
     }
 };
