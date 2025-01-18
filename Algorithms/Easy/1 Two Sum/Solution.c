fa0facd59a4c1232244850105bd826776ed6fe41
MateusVasc <mvaraujo2004@gmail.com>
2025-01-18 19:44:06 -0300

    Adds C solution for problem 1

---
+++ Algorithms/Easy/1 Two Sum/Solution.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* twoSum(int* nums, int numsSize, int target, int* returnSize) 
+{
+    *returnSize = 2;
+    int *outArr = malloc(*returnSize * sizeof(int));
+
+    for (int i = 0; i < numsSize - 1; i++)
+    {
+        for (int j = i + 1; j < numsSize; j++)
+        {
+            if (nums[i] + nums[j] == target)
+            {
+                outArr[0] = i;
+                outArr[1] = j;
+
+                return outArr;
+            }
+            
+        }
+        
+    }
+
+    return outArr;
+}
+
