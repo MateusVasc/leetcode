e5edf8fba92bc09c2b7d9c9a4261ff30ba78aafe
MateusVasc <mvaraujo2004@gmail.com>
2025-03-30 10:57:46 -0300

    adds better solutions

--- hackerrank/algorithms/minimaxsum/MiniMaxSum.java
+++ hackerrank/algorithms/minimaxsum/MiniMaxSum.java
@@ -2,10 +2,11 @@ package hackerrank.algorithms.minimaxsum;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class MiniMaxSum {
 
-    public static void miniMaxSum(List<Integer> arr) {
+    public static void myMiniMaxSum(List<Integer> arr) {
         long min = 0;
         long max = 0;
 
@@ -19,8 +20,45 @@ public class MiniMaxSum {
         System.out.println(min + " " + max);
     }
 
+    public static void streamsMiniMaxSum(List<Integer> arr) {
+        List<Long> arrSorted = arr.stream()
+            .mapToLong(Integer::longValue)
+            .sorted()
+            .boxed()
+            .collect(Collectors.toList());
+
+        long minSum = arrSorted.stream().limit(4).mapToLong(Long::longValue).sum();
+        long maxSum = arrSorted.stream().skip(1).mapToLong(Long::longValue).sum();
+
+        System.out.println(minSum + " " + maxSum);
+    }
+
+    public static void noSortMiniMaxSum(List<Integer> arr) {
+        long sum = 0;
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+
+        for (int num : arr) {
+            sum += num;
+            min = Math.min(min, num);
+            max = Math.max(max, num);
+        }
+
+        System.out.println((sum - max) + " " + (sum - min));
+    }
+
     public static void main(String[] args) {
         List<Integer> arr = new ArrayList<>(List.of(1, 2, 3, 4, 5));
-        miniMaxSum(arr);
+        myMiniMaxSum(arr);
+        streamsMiniMaxSum(arr);
+        noSortMiniMaxSum(arr);
+
+        /*
+         * I'd go with noSortMiniMaxSum solution
+         * 
+         * - It makes no unnecessary sorts
+         * - Has O(n) time complexity
+         * - Pretty concise code with no unnecessary stuff
+         */
     }
 }
