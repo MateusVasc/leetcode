efbbb96f88b0f1d0376021f494a476eb44b02ccf
MateusVasc <mvaraujo2004@gmail.com>
2025-03-29 21:55:41 -0300

    adds better solutions

--- hackerrank/algorithms/staircase/Staircase.java
+++ hackerrank/algorithms/staircase/Staircase.java
@@ -1,8 +1,11 @@
 package hackerrank.algorithms.staircase;
 
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
 public class Staircase {
 
-    public static void staircase(int n) {
+    public static void myStaircaseSolution(int n) {
         char stairChar = '#';
         char stairBlank = ' ';
         StringBuilder stair = new StringBuilder(String.valueOf(stairBlank).repeat(n - 1));
@@ -17,7 +20,50 @@ public class Staircase {
         System.out.println(stair.toString());
     }
 
+    public static void streamsStaircaseSolution(int n) {
+        IntStream.rangeClosed(1, n)
+            .forEach(i -> System.out.println(" ".repeat(n - 1) + "#".repeat(i)));
+    }
+
+    public static void arrStaircaseSolution(int n) {
+        char[] stairs = new char[n];
+        Arrays.fill(stairs, ' ');
+
+        for (int i = n - 1; i >= 0; i--) {
+            stairs[i] = '#';
+            System.out.println(stairs);
+        }
+    }
+
     public static void main(String[] args) {
-        staircase(4);
+        myStaircaseSolution(4);
+        streamsStaircaseSolution(4);
+        arrStaircaseSolution(4);
+
+        /*
+         * My Solution
+         * 
+         * Pros:
+         *  - Uses StringBuilder, which is efficient when it comes to string manipulation
+         *  - Only one update per iteration
+         * Cons:
+         *  - The code could better writen
+         * 
+         * Streams Solution
+         * 
+         * Pros:
+         *  - Just beautiful readable code. Very well writen
+         * Cons:
+         *  - May be less efficient due to the creation of intermediate objects
+         * 
+         * Array Solution
+         * 
+         * Pros:
+         *  - Very memory efficient
+         *  - No intermediate objects
+         *  - Inplace
+         * Cons:
+         *  - Idk tbh... 
+         */
     }
 }
